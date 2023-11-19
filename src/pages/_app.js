54a780cb7c1be4b16c9bc072f6263f533e1df3a1
Anant Batgali <40610958+abatgali@users.noreply.github.com>
2023-11-18 23:06:31 -0500

    Update next.config.js and package.json, and add font and menu components to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import { Cormorant } from "next/font/google";
+import "@/styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
+import { Menu, Input, Button } from "semantic-ui-react";
+import Link from "next/link";
+
+const font = Cormorant({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className={`${font.className}`}>
+        <Menu>
+          <Menu.Item as={Link} href='/'>Home</Menu.Item>
+          <Menu.Item>
+            <Input placeholder='Search...' />
+            <Button as={Link} href='/characters/'>Search</Button>
+          </Menu.Item>
+        </Menu>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
